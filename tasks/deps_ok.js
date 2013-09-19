e2a77a30d2aacb74b00cee4caa52ada95800f88c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-09-19 01:46:29 -0400

    tested failure

--- tasks/deps_ok.js
+++ tasks/deps_ok.js
@@ -14,11 +14,17 @@ module.exports = function(grunt) {
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
+  var txt = 'Quickly checks if top level dependencies' +
+    ' are missing or out of date';
 
-  grunt.registerTask('deps_ok',
-    'Quickly checks if top level dependencies are missing or out of date',
-    function() {
-      var ok = depsOk(process.cwd(), true);
-      grunt.log.writeln(this.name + ', dependencies ok?', ok);
-    });
+  grunt.registerTask('deps_ok', txt, function() {
+    var verbose = false;
+    var ok = depsOk(process.cwd(), verbose);
+    if (!ok) {
+      grunt.log.error('problem with dependencies, probably run `npm install`');
+      return false;
+    }
+
+    grunt.log.writeln('dependencies are ok');
+  });
 };
